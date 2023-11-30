8319978f5df2ad8e8f10c48961f11b4c5766b155
sajjadalidev <iamsajjadali.dev@gmail.com>
2023-11-30 23:51:10 +0500

    Codebase -setting of codebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
 
-function App() {
+import EmailEditor from "react-email-editor";
+
+const App = (props) => {
+  const emailEditorRef = useRef(null);
+
+  const exportHtml = () => {
+    const unlayer = emailEditorRef.current?.editor;
+
+    unlayer?.exportHtml((data) => {
+      const { design, html } = data;
+      alert(JSON.stringify(design, null, 2));
+      console.log("exportHtml", html);
+    });
+  };
+
+  const onReady = (unlayer) => {
+    console.log("I am working", unlayer);
+    // editor is ready
+    // you can load your template here;
+    // the design json can be obtained by calling
+    // unlayer.loadDesign(callback) or unlayer.exportHtml(callback)
+    unlayer.init({
+      id: "editor-container",
+    });
+
+    unlayer.loadDesign({
+      html: "<div>This is a legacy HTML template.</div>",
+      classic: true,
+    });
+
+    // unlayer.loadDesign(template);
+    // unlayer.saveDesign(function (design) {
+    //   console.log("design", design);
+    // });
+
+    // unlayer.addEventListener("design:updated", function (updates) {
+    //   // Design is updated by the user
+
+    //   unlayer.exportHtml(function (data) {
+    //     var json = data.design; // design json
+    //     var html = data.html; // design html
+
+    //     // Save the json, or html here
+    //   });
+    // });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={exportHtml}>Export HTML</button>
+      </div>
+
+      <EmailEditor ref={emailEditorRef} onReady={onReady} />
     </div>
   );
-}
+};
 
 export default App;
